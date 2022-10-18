71c4e785044be2379f5cc0967d493c21b751b07c
radixon <radixon28@yahoo.com>
2022-10-18 17:58:23 -0400

    Add best time to buy and sell stock program in Python.

--- LeetCode/ReadMe.md
+++ LeetCode/ReadMe.md
@@ -2,4 +2,10 @@
 
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. <br />
 <br />
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You may assume that each input would have exactly one solution, and you may not use the same element twice. <br />
+
+# Best Day to Buy and Sell Stock
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.  You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+ <br /><br />   
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
---
+++ LeetCode/test_buysellstock.py
@@ -0,0 +1,24 @@
+import unittest
+import buysellstock
+import buysellstock_arr
+
+class TestProblem1(unittest.TestCase):
+    def test_multiples_of_3_5(self):
+        # Provided Examples
+        self.assertEqual(buysellstock.maxProfit([7,1,5,3,6,4]),5)
+        self.assertEqual(buysellstock.maxProfit([7,6,4,3,1]),0)
+
+        # Arrays generated with random.randint()
+        self.assertEqual(buysellstock.maxProfit(buysellstock_arr.arr0),158)
+        self.assertEqual(buysellstock.maxProfit(buysellstock_arr.arr1),14)
+        self.assertEqual(buysellstock.maxProfit(buysellstock_arr.arr2),91)
+        self.assertEqual(buysellstock.maxProfit(buysellstock_arr.arr3),662)
+        self.assertEqual(buysellstock.maxProfit(buysellstock_arr.arr4),178)
+        self.assertEqual(buysellstock.maxProfit(buysellstock_arr.arr5),0)
+        self.assertEqual(buysellstock.maxProfit(buysellstock_arr.arr6),4)
+
+
+        
+
+if __name__ == '__main__':
+    unittest.main()
