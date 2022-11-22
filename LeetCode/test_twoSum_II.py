10c41642efba29b9d636695ed9f5d4c6233dd139
radixon <radixon28@yahoo.com>
2022-11-22 17:26:46 -0500

    Update two sum II

--- LeetCode/test_twoSum_II.py
+++ LeetCode/test_twoSum_II.py
@@ -9,16 +9,16 @@ class TestTwoSum(unittest.TestCase):
         self.assertEqual(twoSum_II.twoSum(twoSum_II_arr.test2, twoSum_II_arr.target2),twoSum_II_arr.result2)
         self.assertEqual(twoSum_II.twoSum(twoSum_II_arr.test3, twoSum_II_arr.target3),twoSum_II_arr.result3)
 
-        # Edge Case with 0 as part of the sum
+        # General Case
         self.assertEqual(twoSum_II.twoSum(twoSum_II_arr.test4, twoSum_II_arr.target4),twoSum_II_arr.result4)
 
-        # Edge Case with beginning and end of array as part of the sum
+        # Edge Case last two indexes equals target
         self.assertEqual(twoSum_II.twoSum(twoSum_II_arr.test5, twoSum_II_arr.target5),twoSum_II_arr.result5)
 
         # Edge Case with negative number
         self.assertEqual(twoSum_II.twoSum(twoSum_II_arr.test6, twoSum_II_arr.target6),twoSum_II_arr.result6)
 
-        # Edge Case of target value in the array not being in the answer
+        # Edge Case two negative numbers equals the target value
         self.assertEqual(twoSum_II.twoSum(twoSum_II_arr.test7, twoSum_II_arr.target7),twoSum_II_arr.result7)
 
 
