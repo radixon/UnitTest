dbcb847ea1c0a0bc87da78bc8964f0dac1c6858a
Dixon <59415488+radixon@users.noreply.github.com>
2023-02-09 15:32:07 -0500

    Add files via upload

---
+++ LeetCode/0033_SearchInRotatedSortedArray/searchInRotatedSortedArray_arr.py
@@ -0,0 +1,35 @@
+test1 = [4,5,6,7,0,1,2]
+target1 = 0
+result1 = 4
+
+test2 = [4,5,6,7,0,1,2]
+target2 = 3
+result2 = -1
+
+test3 = [1]
+target3 = 0
+result3 = -1
+
+test4 = [16, 19, 27, 35, 41, 45, 49, 50, -57, -55, -51, -49, -10, 4, 7]
+target4 = 45
+result4 = 5
+
+test5 = [5,1,3]
+target5 = 5
+result5 = 0
+
+test6 = [3,1]
+target6 = 1
+result6 = 1
+
+test7 = [1]
+target7 = 1
+result7 = 0
+
+test8 = [4,5,6,7,0,1,2]
+target8 = 5
+result8 = 1
+
+test9 = [4,5,6,7,0,1,2]
+target9 = 0
+result9 = 4
