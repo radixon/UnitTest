bd5d9795c8bd02bbced5074d4b349e1f06aa702e
Dixon <59415488+radixon@users.noreply.github.com>
2023-07-26 23:03:33 -0400

    Update test_twoSum_II.py
    Use a for loop, so updating the unit test can be done more seamlessly.

--- TwoPointer/001TwoSumII/test_twoSum_II.py
+++ TwoPointer/001TwoSumII/test_twoSum_II.py
@@ -4,23 +4,10 @@ import twoSum_II_arr
 
 class TestTwoSum(unittest.TestCase):
     def test_twoSum(self):
-        # Provided Examples
-        self.assertEqual(twoSum_II.twoSum(twoSum_II_arr.test1, twoSum_II_arr.target1),twoSum_II_arr.result1)
-        self.assertEqual(twoSum_II.twoSum(twoSum_II_arr.test2, twoSum_II_arr.target2),twoSum_II_arr.result2)
-        self.assertEqual(twoSum_II.twoSum(twoSum_II_arr.test3, twoSum_II_arr.target3),twoSum_II_arr.result3)
-
-        # General Case
-        self.assertEqual(twoSum_II.twoSum(twoSum_II_arr.test4, twoSum_II_arr.target4),twoSum_II_arr.result4)
-
-        # Edge Case last two indexes equals target
-        self.assertEqual(twoSum_II.twoSum(twoSum_II_arr.test5, twoSum_II_arr.target5),twoSum_II_arr.result5)
-
-        # Edge Case with negative number
-        self.assertEqual(twoSum_II.twoSum(twoSum_II_arr.test6, twoSum_II_arr.target6),twoSum_II_arr.result6)
-
-        # Edge Case two negative numbers equals the target value
-        self.assertEqual(twoSum_II.twoSum(twoSum_II_arr.test7, twoSum_II_arr.target7),twoSum_II_arr.result7)
-
+        # Examples in twoSum_II_arr.py file
+        for i in range(len(twoSum_II_arr.test)):
+            self.assertEqual(twoSum_II.twoSum(twoSum_II_arr.test[i], twoSum_II_arr.target[i]),twoSum_II_arr.result[i])
+        
 
 if __name__ == '__main__':
     unittest.main()
