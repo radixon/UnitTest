cdc515c5a4f32ad6ea8f5910233f09e2ebb752c4
Dixon <59415488+radixon@users.noreply.github.com>
2023-08-02 00:30:48 -0400

    Update binarySearch.py
    Added example cases

--- BinarySearch/001BinarySearch/binarySearch.py
+++ BinarySearch/001BinarySearch/binarySearch.py
@@ -16,3 +16,14 @@ def search(nums, target):
         else:
             high = mid - 1
     return -1
+
+
+if __name__ == '__main__':
+    nums = [[-1,0,3,5,9,12],
+            [-1,0,3,5,9,12]
+           ]
+    
+    target = [9,2]
+
+    for i in range(len(nums)):
+        print(search(nums[i],target[i]))
