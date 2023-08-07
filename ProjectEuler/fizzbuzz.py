a6d1fa140a9ecdf6335da4583048c884ff525f11
Dixon <59415488+radixon@users.noreply.github.com>
2023-08-07 09:56:06 -0400

    Delete ProjectEuler directory

--- ProjectEuler/fizzbuzz.py
+++
@@ -1,7 +0,0 @@
-def fizzbuzz(num):
-    """
-    If a number is a multiple of 3 the answer is Fizz.  If a number is a multiple of 5 the answer is Buzz.
-    If a number is a multiple of both 3 and 5 the answer is FizzBuzz.  Otherwise the answer is the value.
-    """
-    ans = ((num != 0)*(num%3 == 0)*"Fizz" + (num != 0)*(num%5 == 0)*"Buzz") or num
-    return ans 
