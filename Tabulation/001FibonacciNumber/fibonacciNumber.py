a40597aac9ca088c89516ab1734bc9fe34b14c4e
Dixon <59415488+radixon@users.noreply.github.com>
2023-08-08 16:29:06 -0400

    Add time/space complexity to fibonacciNumber.py

--- Tabulation/001FibonacciNumber/fibonacciNumber.py
+++ Tabulation/001FibonacciNumber/fibonacciNumber.py
@@ -1,4 +1,11 @@
 class Solution():
+    """
+    Time Complexity: O(n)
+    The algorithm will loop n times.
+
+    Space Complexity: O(1)
+    Constant extra space is used for res, curr, and prev variables.
+    """
     def fib(self,n):
         res, curr = 0, 1
         for _ in range(n):
