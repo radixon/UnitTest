0e492e4918c35dc7cb9768b7798ba6244090c75b
Dixon <59415488+radixon@users.noreply.github.com>
2023-08-15 23:34:08 -0400

    Add files via upload

---
+++ BinarySearch/003FirstBadVersion/firstBadVersion_test.py
@@ -0,0 +1,20 @@
+import unittest
+import firstBadVersion
+import firstBadVersion_cases
+
+class TestFirstBadVersion(unittest.TestCase):
+    def test_firstBadVersion(self):
+        # Test Cases can be found in firstBadVersion_cases.py
+        n = firstBadVersion_cases.n
+        bad = firstBadVersion_cases.bad
+        for i in range(len(n)):
+            case = firstBadVersion.Solution(bad[i])
+            try: 
+                self.assertEqual(case.firstBadVersion(n[i]),bad[i])
+            except AssertionError:
+                print("Failed Case")
+                print('n: ', n[i],'\t','bad: ',bad[i],'\n')
+        
+
+if __name__ == '__main__':
+    unittest.main()
