710fb3075f89eba0528c415df9867343e96eddd1
Dixon <59415488+radixon@users.noreply.github.com>
2023-08-15 23:37:07 -0400

    Update fibNumber_test.py

--- Memoization/001FibonacciNumber/fibNumber_test.py
+++ Memoization/001FibonacciNumber/fibNumber_test.py
@@ -7,7 +7,11 @@ class TestFibNumber(unittest.TestCase):
         # Test Cases can be found in fibNumber_cases.py
         case = fibonacciNumber.Solution()
         for i in range(len(fibNumber_cases.n)):
-            self.assertEqual(case.fib(fibNumber_cases.n[i]),fibNumber_cases.res[i])
+            try:
+                self.assertEqual(case.fib(fibNumber_cases.n[i]),fibNumber_cases.res[i])
+            except AssertionError:
+                print("Failed Case")
+                print('n: ', fibNumber_cases.n[i], '\t', 'result: ', fibNumber_cases.res[i], '\n')
         
 
 if __name__ == '__main__':
